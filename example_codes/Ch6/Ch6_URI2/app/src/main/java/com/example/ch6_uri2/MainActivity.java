46361628c7f92e190fe85461f6c02ccd5ba81a7b
Jinwooseol <jinwooseol@postech.ac.kr>
2024-05-07 18:09:56 +0900

    Ch6 복습 내용 추가

--- example_codes/Ch6/Ch6_URI2/app/src/main/java/com/example/ch6_uri2/MainActivity.java
+++ example_codes/Ch6/Ch6_URI2/app/src/main/java/com/example/ch6_uri2/MainActivity.java
@@ -6,16 +6,55 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText webSiteEditText;
+    TextView schemeTextView, hostTextView, portTextView, pathTextView;
+    Button button;
+    WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Uri uri = Uri.parse("https://gw.optolane.com/login?returnUrl=%2Fapp%2Fhome");
-        Intent
-        Log.d("URI", "Uri : " + intent.getData());
-        getContent
+        webSiteEditText = findViewById(R.id.webSiteEditText);
+        schemeTextView = findViewById(R.id.schemeTextView);
+        hostTextView = findViewById(R.id.hostTextView);
+        portTextView = findViewById(R.id.portTextView);
+        pathTextView = findViewById(R.id.pathTextView);
+        button = findViewById(R.id.button);
+        webView = findViewById(R.id.webView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String webSite = webSiteEditText.getText().toString();
+
+                if (webSite != null) {
+                    Uri uri = Uri.parse(webSite);
+                    URL webPageURL = null;
+                    String uriScheme = uri.getScheme();
+                    String uriHost = uri.getHost();
+                    int uriPort = uri.getPort();
+                    String uriPath = uri.getPath();
+                    schemeTextView.setText("Scheme : " + uriScheme);
+                    hostTextView.setText("Host : " + uriHost);
+                    portTextView.setText("Port : " + uriPort);
+                    pathTextView.setText("Path : " + uriPath);
+
+                    webView.setWebViewClient(new WebViewClient());
+                    webView.loadUrl(webSite);
+
+                }
+            }
+        });
     }
 }
