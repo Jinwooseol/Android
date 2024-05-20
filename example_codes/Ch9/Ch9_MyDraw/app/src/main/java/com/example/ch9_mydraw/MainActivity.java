d48f0969daabbf2090b5dcffb15aa06f1e9fb28e
Jinwooseol <Jinwooseol@github.com>
2024-05-21 08:06:10 +0900

    Ch 9 & 10 내용 추가

---
+++ example_codes/Ch9/Ch9_MyDraw/app/src/main/java/com/example/ch9_mydraw/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ch9_mydraw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private SingleTouchView drawView;
+    private ImageButton currPaint;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawView = findViewById(R.id.drawing);
+        LinearLayout paintLayout = findViewById(R.id.paint_colors);
+        currPaint = (ImageButton) paintLayout.getChildAt(0);
+    }
+    public void clicked(View view) {
+        if (view != currPaint) {
+            String color = view.getTag().toString();
+            drawView.setColor(color);
+            currPaint = (ImageButton) view;
+        }
+    }
+}
