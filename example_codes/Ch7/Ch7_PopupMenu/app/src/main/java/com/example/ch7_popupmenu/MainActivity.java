0612bc5b433fb521fc95bc8b10a2924dadeadb76
Jinwooseol <jinwooseol@postech.ac.kr>
2024-05-21 21:20:21 +0900

    내용 정리

--- example_codes/Ch7/Ch7_PopupMenu/app/src/main/java/com/example/ch7_popupmenu/MainActivity.java
+++ example_codes/Ch7/Ch7_PopupMenu/app/src/main/java/com/example/ch7_popupmenu/MainActivity.java
@@ -3,32 +3,59 @@ package com.example.ch7_popupmenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private boolean isSubMenu = false;
+    private View anchorButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        anchorButton = findViewById(R.id.button);
     }
 
-    public void onClick(View button) {
-        PopupMenu popup = new PopupMenu(this, button);
-        popup.getMenuInflater().inflate(R.menu.popup, popup.getMenu());
+    public void onClick(View view) {
+        showPopupMenu();
+    }
+
+    private void showPopupMenu() {
+        PopupMenu popup = new PopupMenu(this, anchorButton);
         popup.setForceShowIcon(true);
         popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                                              @Override
                                              public boolean onMenuItemClick(MenuItem item) {
-                                                 Toast.makeText(getApplicationContext(), "클릭된 팝업 메뉴: " + item.getTitle(),
-                                                         Toast.LENGTH_SHORT).show();
-                                                 return true;
+                                                 return handleMenuItemClick(item);
                                              }
                                          }
         );
+        if (isSubMenu) {
+            popup.getMenuInflater().inflate(R.menu.popup_submenu, popup.getMenu());
+        } else {
+            popup.getMenuInflater().inflate(R.menu.popup, popup.getMenu());
+        }
         popup.show();
+    }
+
+    private boolean handleMenuItemClick(MenuItem item) {
+        if (item.getItemId() == R.id.edit) {
+            isSubMenu = true;
+            showPopupMenu();
+            return true;
+        } else if (item.getItemId() == R.id.edit_new) {
+            isSubMenu = false;
+            showPopupMenu();
+            return true;
+        } else if (item.getItemId() == R.id.add ||
+                item.getItemId() == R.id.search ||
+                item.getItemId() == R.id.share) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
