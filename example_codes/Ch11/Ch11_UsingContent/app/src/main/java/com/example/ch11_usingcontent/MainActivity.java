f985735fd2e9806fcfcf4e27718789802f38d39c
Jinwooseol <Jinwooseol@github.com>
2024-05-29 07:41:42 +0900

    Ch11 & 12 내용 추가

--- example_codes/Ch11/Ch11_UsingContent/app/src/main/java/com/example/ch11_usingcontent/MainActivity.java
+++ example_codes/Ch11/Ch11_UsingContent/app/src/main/java/com/example/ch11_usingcontent/MainActivity.java
@@ -8,12 +8,8 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import android.Manifest;
 import android.widget.Toast;
@@ -26,33 +22,67 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.picture);
         ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
     }
     public void displayFirstImage(View v) {
-        Toast.makeText(getApplicationContext(), "displayFirstImage()",
-                Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "displayFirstImage()", Toast.LENGTH_LONG).show();
         try {
-            String[] projection = new String[] {
+            String[] projection = new String[]{
                     MediaStore.Images.ImageColumns._ID,
                     MediaStore.Images.ImageColumns.DATA
             };
             cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                    projection, null, null, null);
+                    projection,
+                    null,
+                    null,
+                    null);
+
             int size = cursor.getCount();
             if (size == 0) {
-                Toast.makeText(getApplicationContext(), "장치에 이미지가 없음!",
-                         Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "장치에 이미지가 없음!", Toast.LENGTH_LONG).show();
             } else {
                 if (cursor.moveToFirst()) {
                     String imageLocation = cursor.getString(1);
-                    Toast.makeText(getApplicationContext(), imageLocation,
-                            Toast.LENGTH_LONG).show();
-                    File imgaeFile = new File(imageLocation);
-                    if (imgaeFile.exists()) {
+                    Toast.makeText(getApplicationContext(), imageLocation, Toast.LENGTH_LONG).show();
+                    File imageFile = new File(imageLocation);
+                    if (imageFile.exists()) {
+                        Bitmap bm = BitmapFactory.decodeFile(imageLocation);
+                        imageView.setImageBitmap(bm);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void displaySecondImage(View v) {
+        Toast.makeText(getApplicationContext(), "displaySecondImage()", Toast.LENGTH_LONG).show();
+        try {
+            String[] projection = new String[]{
+                    MediaStore.Images.ImageColumns._ID,
+                    MediaStore.Images.ImageColumns.DATA,
+                    MediaStore.Images.ImageColumns.BUCKET_DISPLAY_NAME,
+                    MediaStore.Images.ImageColumns.DATE_TAKEN,
+                    MediaStore.Images.ImageColumns.MIME_TYPE
+            };
+            cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    projection,
+                    null,
+                    null,
+                    null);
+
+            int size = cursor.getCount();
+            if (size == 0) {
+                Toast.makeText(getApplicationContext(), "장치에 이미지가 없음!", Toast.LENGTH_LONG).show();
+            } else {
+                if (cursor.moveToLast()) {
+                    String imageLocation = cursor.getString(1);
+                    Toast.makeText(getApplicationContext(), imageLocation, Toast.LENGTH_LONG).show();
+                    File imageFile = new File(imageLocation);
+                    if (imageFile.exists()) {
                         Bitmap bm = BitmapFactory.decodeFile(imageLocation);
                         imageView.setImageBitmap(bm);
                     }
